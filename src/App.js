01e6e82b5a9ecf1799d98a7de6c36803a630980c
lucassherwin <lksherwin@gmail.com>
2021-01-13 16:55:57 -0500

    results list now displays with nominate button

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import Results from './components/Results.js';
 import axios from 'axios';
 class App extends Component {
   state = {
-    nominations: null, // list of all the nominations
+    nominations: [], // list of all the nominations
     search: '',
-    results: null
+    results: []
   }
 
   nominate = (event) => {
@@ -18,12 +18,26 @@ class App extends Component {
   handleSearch = (event) => {
     const key = process.env.REACT_APP_OMBD_API_KEY;
     console.log('search: ', event.target.value);
-
+    let term = event.target.value;
     let { name, value } = event.target;
     this.setState({[name]: value});
 
-    axios.get(`http://www.omdbapi.com/?apikey=${key}&t=${this.state.search}`)
-    .then(resp => console.log(resp))
+    let resultString;
+    let searchResults = this.state.results;
+
+    axios.get(`http://www.omdbapi.com/?apikey=${key}&t=${term}`)
+    .then(res => {
+      if(res.data['Response'] !== "False")
+      {
+          resultString = `${res.data['Title']} (${res.data['Year']})`
+          if(!searchResults.includes(resultString)) //if it doesnt already include the value
+          {
+              searchResults.push(resultString); //push the whole movie obj that is returned into the array
+              // console.log('search results: ', searchResults);
+              this.setState({searchResults}); //set it to state
+          }
+      }
+  })
   }
 
 
@@ -33,8 +47,8 @@ class App extends Component {
       <div>
         <h1>The Shoppies</h1>
         <Search search={this.state.search} handleSearch={this.handleSearch} />
-        {this.state.nominations ? <Nominations /> : null}
-        {this.state.results ? <Results /> : null}
+        {this.state.nominations.length !== 0 ? <Nominations /> : null}
+        {this.state.results.length !== 0 ? <Results results={this.state.results} search={this.state.search} /> : null}
       </div>
       
     )
--- src/components/Results.js
+++ src/components/Results.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
-export default function Results() {
+export default function Results(props) {
     return (
         <div>
-            <h2>Results for [search string]</h2>
+            <h2>Resluts for {props.search}</h2>
+            <ul>
+                {props.results.map((movie, index) => (
+                    <li key={index}>{movie} <button>Nominate!</button></li>
+                ))}
+            </ul>
         </div>
     )
 }
