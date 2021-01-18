7d8a7799c812f7b3727074eca921f804afe0472a
lucassherwin <lksherwin@gmail.com>
2021-01-18 11:52:46 -0500

    added button to navigate use to login

--- src/components/ShopImg.js
+++ src/components/ShopImg.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
 import axios from 'axios';
+import { Redirect } from 'react-router-dom';
 
 export class ShopImg extends Component {
     state = {
-        images: null
+        images: null,
+        redirect: null
     }
 
     getImages = (posts) => {
@@ -17,6 +19,10 @@ export class ShopImg extends Component {
         this.setState({images})
     }
 
+    handleClick = () => {
+        this.setState({redirect: '/login'})
+    }
+
     componentDidMount() {
         axios.get('http://localhost:3001/posts')
         .then(resp => this.getImages(resp.data))
@@ -24,10 +30,15 @@ export class ShopImg extends Component {
 
 
     render() {
+        if(this.state.redirect)
+        {
+            return(<Redirect to={this.state.redirect} />)
+        }
         return (
             <div>
                 <h1>Shop IMG</h1>
                 <h2>The Shopify Image Repository</h2>
+                <button onClick={this.handleClick}>Create Account</button>
                 <div>
                     {this.state.images ? this.state.images.map((image, index) => (
                         <img src={`http://localhost:3001/${image}`} alt='alt' key={index} />
